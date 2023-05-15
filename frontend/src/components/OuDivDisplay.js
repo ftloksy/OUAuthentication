f168132528e1ec866d617f3244dea7427e89584e
Frankie Chow <freakchow@gmail.com>
2023-05-15 13:14:58 +0100

    add Fist components for FrontEnd.

---
+++ frontend/src/components/OuDivDisplay.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class OuDivDisplay extends Component {
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+
+      return (
+        <h1>OuDiv</h1>
+      );
+  }
+}
+
+export default OuDivDisplay;
